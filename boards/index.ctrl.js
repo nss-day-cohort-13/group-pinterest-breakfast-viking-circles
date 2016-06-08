f7334034d7155bf6be16c5e7f04638889a389b60
Sulaiman.Allen <xalerons@gmail.com>
2016-06-08 12:30:44 -0500

    General cleanup

--- boards/index.ctrl.js
+++ boards/index.ctrl.js
@@ -4,10 +4,6 @@ angular.module('app')
   .controller('BoardCtrl', function (BoardFactory) {
     const board = this;
 
-    // BoardFactory.all().then(list =>
-    //   board.list = list
-    // );
-
     BoardFactory.all().then(function (list) {
       board.list = list;
       }
@@ -21,13 +17,4 @@ angular.module('app')
         ...board.list.slice(index + 1)
       ];
     };
-    
-    // board.delete = (index) => {
-    //   BoardFactory.delete(index);
-
-    //   board.list = [
-    //     ...board.list.slice(0, index),
-    //     ...board.list.slice(index + 1)
-    //   ];
-    // };
   });
--- boards/new.ctrl.js
+++ boards/new.ctrl.js
@@ -7,12 +7,10 @@ angular.module('app')
     board.submit = function () {
       // if this is a board creation
       if (board.url === undefined) {
-          console.log("This should be a board");
           BoardFactory.boardCreate(board);
           $location.path('/boards');
       // if this is a pin creation
       } else { 
-          console.log("this should be a pin");
           BoardFactory.pinCreate(board);
           $location.path('/boards/');
       }
